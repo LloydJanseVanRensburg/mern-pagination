b59bf9cbfa1a3fb3922b0d25482215983d429998
Abhinav4952 <abhinav.4952@gmail.com>
2021-07-03 20:21:05 +0530

    initial heroku script

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config({ path: "./config.env" });
+const path = require('path')
 const express = require("express");
 const connectDB = require("./config/db");
 const postRoutes = require("./routes/postRoutes");
@@ -8,8 +9,19 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-
+//https://git.heroku.com/enigmatic-temple-25922.git
+//https://enigmatic-temple-25922.herokuapp.com/
 app.use("/api/v1/posts", postRoutes);
-
+console.log("buidl path", path.join(__dirname,'/client/build'));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,'/client/build')));
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    });
+}else{
+    app.get('/',(req,res) => {
+        res.send("Api running")
+    }); 
+}
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
