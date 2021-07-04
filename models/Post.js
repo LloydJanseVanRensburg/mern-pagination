66aca2405611259793b4e04220227902816d93d9
Abhinav Veeramosu <abhinav.veeramosu@ehealthCorp.net>
2021-07-04 23:30:41 +0530

    Update passwod sfield

--- models/Post.js
+++ models/Post.js
@@ -37,8 +37,7 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, "Please add a password"],
-    minlength: 6,
+    minlength: 8,
     select: false,
   },
   userType: { 
